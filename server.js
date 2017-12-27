17bb5c87359619d71645a6d16ab62701a8338f5d
Hamzeen. H <hamzeen@gmail.com>
2017-12-27 14:11:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // grab the packages we need
 var express = require('express');
 var app = express();
@@ -7,57 +8,30 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
 
-// routes will go here
-
-// ====================================
-// URL PARAMETERS =====================
-// ====================================
-// http://localhost:8080/api/users?id=4&token=sadsf4&geo=us
-app.get('/api/users', function(req, res) {
-  var user_id = req.param('id');
-  var token = req.param('token');
-  var geo = req.param('geo');  
-
-  res.send(user_id + ' ' + token + ' ' + geo);
-});
-
-// http://localhost:8080/api/1
-app.get('/api/:version', function(req, res) {
-	res.send(req.params.version);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // parameter middleware that will run before the next routes
-app.param('name', function(req, res, next, name) {
+/*app.param('name', function(req, res, next, name) {
 
-	// check if the user with that name exists
-	// do some validations
-	// add -dude to the name
-	var modified = name + '-dude';
+    // check if the user with that name exists
+    // do some validations
+    // add -dude to the name
+    var modified = name + '-dude';
 
-	// save name to the request
-	req.name = modified;
+    // save name to the request
+    req.name = modified;
+    next();
+});*/
 
-	next();
-});
-
-// http://localhost:8080/api/users/chris
-app.get('/api/users/:name', function(req, res) {
-	// the user was found and is available in req.user
-	res.send('What is up ' + req.name + '!');
-});
-
-// ====================================
-// POST PARAMETERS ====================
-// ====================================
-
-// POST http://localhost:8080/api/users
-// parameters sent with 
-app.post('/api/users', function(req, res) {
-	var user_id = req.body.id;
-	var token = req.body.token;
-	var geo = req.body.geo;
-
-	res.send(user_id + ' ' + token + ' ' + geo);
+// [POST] http://localhost:8080/api/logs
+app.post('/api/logs', function(req, res) {
+    //res.send('response::' + req.body.message);
+    res.sendStatus(200);
+    console.log("received:" + req.body.message);
 });
 
 // start the server
