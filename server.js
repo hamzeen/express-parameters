95b7dc728ab3487504527a80220925d04a1f462d
Hamzeen. H <hamzeen@gmail.com>
2017-12-27 14:49:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,30 +10,25 @@ app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
 });
 
-// parameter middleware that will run before the next routes
-/*app.param('name', function(req, res, next, name) {
-
-    // check if the user with that name exists
-    // do some validations
-    // add -dude to the name
-    var modified = name + '-dude';
-
-    // save name to the request
-    req.name = modified;
-    next();
-});*/
 
 // [POST] http://localhost:8080/api/logs
 app.post('/api/logs', function(req, res) {
-    //res.send('response::' + req.body.message);
-    res.sendStatus(200);
+    res.send('response::' + req.body.message);
+    // res.sendStatus(200);
     console.log("received:" + req.body.message);
 });
 
 // start the server
 app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+console.log('magic happens at http://localhost:' + port);
